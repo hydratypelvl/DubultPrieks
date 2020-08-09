1f479cda8bb72afba541c40576fabc91f07125d0
Rihards <hydratypelvl@gmail.com>
2020-08-10 00:11:37 +0300

    create admin Laravel command
    Added command that adds Admin User to Database

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,19 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+//Generates Admin User
+Artisan::command('admin:create', function () {
+    $name = 'Admin';
+    $email = 'admin@admin.com';
+    $pwd = Hash::make('qwertyqwe');
+    // $pwd = $this->secret('123'); // or use secret() to hide the password being inputted
+    \DB::table('users')->insert([
+            'name' => $name,
+            'email' => $email,
+            'password' => $pwd,
+            'created_at' => date_create()->format('Y-m-d H:i:s'),
+            'updated_at' => date_create()->format('Y-m-d H:i:s'),
+    ]);
+    $this->info('Admin user Created Successfully');
+});
